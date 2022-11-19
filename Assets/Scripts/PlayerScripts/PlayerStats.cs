3bbb4658dcb45de3bd7738baeb98396e54b77b3f
William <willcallihan13@gmail.com>
2022-11-19 17:45:27 -0600

    preliminary potion and stat creation
    created player stats and potion scriptable objects

---
+++ Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerStat { MaxHealth, AttackPower, Defense, Speed, AttackSpeed }
+
+[CreateAssetMenu (menuName = "PlayerStats")]
+public class PlayerStats : ScriptableObject {
+
+    [SerializeField] private float maxHealthBase;
+    [SerializeField, ReadOnly] private float maxHealth;
+    [SerializeField] private float attackPowerBase;
+    [SerializeField, ReadOnly] private float attackPower;
+    [SerializeField] private float defenseBase;
+    [SerializeField, ReadOnly] private float defense;
+    [SerializeField] private float speedBase;
+    [SerializeField, ReadOnly] private float speed;
+    [SerializeField] private float attackSpeedBase;
+    [SerializeField, ReadOnly] private float attackSpeed;
+
+    private float maxHealthPercentageChange = 0;
+    private float attackPowerPercentageChange = 0;
+    private float defensePercentageChange = 0;
+    private float speedPercentageChange = 0;
+    private float attackSpeedPercentageChange = 0;
+
+    public float MaxHealth => maxHealth;
+    public float AttackPower => attackPower;
+    public float Defense => defense;
+    public float Speed => speed;
+    public float AttackSpeed => attackSpeed;
+
+    //call at the beginning of a dungeon/scene to reset the stats to base levels
+    public void InitializeStats() {
+        maxHealth = maxHealthBase;
+        attackPower = attackPowerBase;
+        defense = defenseBase;
+        speed = speedBase;
+        attackSpeed = attackSpeedBase;
+    }
+
+    public void ChangeMaxHealth(float percentageChange) {
+        maxHealthPercentageChange += percentageChange;
+        maxHealth = maxHealthBase + (maxHealthBase * (maxHealthPercentageChange / 10));
+    }
+
+    public void ChangeAttackPower(float percentageChange) {
+        attackPowerPercentageChange += percentageChange;
+        attackPower = attackPowerBase + (attackPowerBase * (attackPowerPercentageChange / 10));
+    }
+
+    public void ChangeDefense(float percentageChange) {
+        defensePercentageChange += percentageChange;
+        defense = defenseBase + (defenseBase * (defensePercentageChange / 10));
+    }
+
+    public void ChangeSpeed(float percentageChange) {
+        speedPercentageChange += percentageChange;
+        speed = speedBase + (speedBase * (speedPercentageChange / 10));
+    }
+
+    public void ChangeAttackSpeed(float percentageChange) {
+        attackSpeedPercentageChange += percentageChange;
+        attackSpeed = attackSpeedBase + (attackSpeedBase * (attackSpeedPercentageChange / 10));
+    }
+}
---
+++ Assets/Scripts/PotionScripts/PotionBase.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Potion")]
+public class PotionBase : ScriptableObject {
+
+    [SerializeField] private PlayerStat statToBuff;
+    [SerializeField] private float buffPercentage;
+    [SerializeField] private PlayerStat statToDebuff;
+    [SerializeField] private float debuffPercentage;
+
+    //TODO: set up how to display this information to the player before they take (strings, images, etc)
+    //          (this could be done on another manager, but idk)
+
+
+    //TODO: play around with this and figure out how to pass in the stats most efficiently
+    public void ApplyPotion(PlayerStats player) {
+        AdjustStat(player, statToBuff, buffPercentage);
+        AdjustStat(player, statToDebuff, -debuffPercentage);
+    }
+
+    private void AdjustStat(PlayerStats player, PlayerStat stat, float percentage) {
+        switch(stat) {
+            case PlayerStat.MaxHealth:
+                player.ChangeMaxHealth(percentage);
+                break;
+            case PlayerStat.AttackPower:
+                player.ChangeAttackPower(percentage);
+                break;
+            case PlayerStat.Defense:
+                player.ChangeDefense(percentage);
+                break;
+            case PlayerStat.Speed:
+                player.ChangeSpeed(percentage);
+                break;
+            case PlayerStat.AttackSpeed:
+                player.ChangeAttackSpeed(percentage);
+                break;
+        }
+    }
+}
