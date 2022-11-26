fee5873d4a5128c2b9029338e5d6960eb8cb4378
William <willcallihan13@gmail.com>
2022-11-26 11:00:22 -0600

    make attack delay do something

--- Assets/Scripts/AttackScripts/Attack.cs
+++ Assets/Scripts/AttackScripts/Attack.cs
@@ -15,10 +15,10 @@ public class Attack : MonoBehaviour
 	[SerializeField, ShowIf("_useDefaultAttackSettings")] protected AttackSettings _settings;
 
 	[SerializeField] bool _useCustomAttackSettings = false;
-	[SerializeField, ShowIf("_useCustomAttackSettings")] float _meleeDamage;
-	[SerializeField, ShowIf("_useCustomAttackSettings")] float _timeBetweenAttacks;
-	[SerializeField, ShowIf("_useCustomAttackSettings")] LayerMask _includeLayers;
-	[SerializeField, ShowIf("_useCustomAttackSettings")] float _attackRange;
+	[SerializeField, ShowIf("_useCustomAttackSettings")] float _customMeleeDamage;
+	[SerializeField, ShowIf("_useCustomAttackSettings")] float _customTimeBetweenAttacks;
+	[SerializeField, ShowIf("_useCustomAttackSettings")] LayerMask _customIncludeLayers;
+	[SerializeField, ShowIf("_useCustomAttackSettings")] float _customAttackRange;
    
 	[SerializeField] bool _usePlayerStats = false;
 	[SerializeField, ShowIf("_usePlayerStats")] private PlayerStats _playerStats;
@@ -35,7 +35,7 @@ public class Attack : MonoBehaviour
 	{
 		get 
 		{
-			if(_useCustomAttackSettings) return _meleeDamage;
+			if(_useCustomAttackSettings) return _customMeleeDamage;
 			else if (_usePlayerStats) return _playerStats.AttackPower;
 			else return _settings.MeleeDamage;
 		}
@@ -45,7 +45,7 @@ public class Attack : MonoBehaviour
 	{
 		get 
 		{
-			if(_useCustomAttackSettings) return _timeBetweenAttacks;
+			if(_useCustomAttackSettings) return _customTimeBetweenAttacks;
 			else if(_usePlayerStats) return _playerStats.AttackDelay;
 			else return _settings.AttackDelay;
 		}
@@ -55,7 +55,7 @@ public class Attack : MonoBehaviour
    {
       get 
       {
-         if (_useCustomAttackSettings) return _includeLayers;
+         if (_useCustomAttackSettings) return _customIncludeLayers;
          else return _settings.IncludeLayers;
       }
    }
@@ -64,12 +64,12 @@ public class Attack : MonoBehaviour
    {
       get 
       {
-         if (_useCustomAttackSettings) return _attackRange;
+         if (_useCustomAttackSettings) return _customAttackRange;
          else return _settings.AttackRange;
       }
    }
 
-   [SerializeField, ReadOnly] float _timeBetweenAttack;
+   [SerializeField, ReadOnly] float _timeSinceLastAttack;
 
     private void Awake()
     {
@@ -78,36 +78,13 @@ public class Attack : MonoBehaviour
 
     void Update()
     {
-        if (_timeBetweenAttack <= 0)
-        {
-            if (_settings.UserType == UserType.Player) // If the user is a player
-            {
-                //if (Input.GetMouseButtonDown(0))// and if the player presses the left mouse button
-                //{
-                //    // Debug.Log("REE");
-                //    HandlePrimaryAttack(); // it will do the attack
-                //    _timeBetweenAttack = TimeBetweenAttacks; // and reset the clock
-
-                //}
-                //else if (Input.GetMouseButtonDown(1)) // if the player presses the left mouse button
-                //{
-                //    HandleSecondaryAttack(); // it'll do the secondary attack
-                //}
-            }
-            else // But if the user isn't a player
-            {
-                // HandlePrimaryAttack(); // it'll only run the primary attack logic
-                // _timeBetweenAttack = TimeBetweenAttacks; // and then reset the clock
-            }
-        }
-        else
-        {
-            _timeBetweenAttack -= Time.deltaTime;
-        }
+		_timeSinceLastAttack += Time.deltaTime;
     }    
 
     public virtual void HandlePrimaryAttack()
     {
+		if(_timeSinceLastAttack < TimeBetweenAttacks) return;
+		
         _onPrimaryAttack?.Invoke();
         Collider2D[] targets = Physics2D.OverlapCircleAll(_attackPosition.position, AttackRange, IncludeLayers);
         foreach (Collider2D collider in targets)
@@ -115,6 +92,7 @@ public class Attack : MonoBehaviour
             // Debug.Log(collider.name); 
             HandleHitLogic(collider);
         }
+		_timeSinceLastAttack = 0;
     }
 
     //Dash mechanic
