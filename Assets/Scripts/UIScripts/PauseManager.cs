a55831272b7dcd452db354672a383cc658878973
Jonathan Gonzalez <jonathansgonz03@gmail.com>
2022-11-27 00:53:31 -0600

    MainMenu pause menu added

---
+++ Assets/Scripts/UIScripts/PauseManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool CanPause { get; set; } = false;
+
+    public KeyCode pauseKeycode = KeyCode.Escape;
+    public KeyCode secondaryPauseKeycode = KeyCode.P;
+    public GameObject childObject;
+
+    private static bool isPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused)
+            childObject.SetActive(true);
+        else
+            childObject.SetActive(false);
+
+        if (!CanPause)
+            return;
+
+        if ((Input.GetKeyDown(pauseKeycode) || Input.GetKeyDown(secondaryPauseKeycode)) && !isPaused)
+            PauseResponse();
+
+    }
+
+    public void PauseResponse()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void ResumeButton()
+    {
+        PauseResponse();
+    }
+
+    public void RestartGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
