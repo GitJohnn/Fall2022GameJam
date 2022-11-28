0449fa2c23a98aad24fbdff1e5a711eeecf6e963
Brandon Coffey <bmc939@gmail.com>
2022-11-27 21:56:45 -0600

    Toggle Pause :)

--- Assets/Scripts/UIScripts/PauseManager.cs
+++ Assets/Scripts/UIScripts/PauseManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -28,8 +28,8 @@ public class PauseManager : MonoBehaviour
         if (!CanPause)
             return;
 
-        if ((Input.GetKeyDown(pauseKeycode) || Input.GetKeyDown(secondaryPauseKeycode)) && !isPaused)
-            PauseResponse();
+	    if ((Input.GetKeyDown(pauseKeycode) || Input.GetKeyDown(secondaryPauseKeycode)))
+		    PauseResponse();
 
     }
 
